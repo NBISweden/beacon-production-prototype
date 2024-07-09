f81a82919bad0999a0d1205dccd48ee65f568bb7
Oriol Lopez-Doriga <orildsaga@gmail.com>
2024-07-09 13:13:09 +0200

    unit tests done

--- beacon/__main__.py
+++ beacon/__main__.py
@@ -5,16 +5,31 @@ from beacon.conf.conf import level
 import asyncio
 import aiohttp.web as web
 
+def calculate(nombre):
+    try:
+        status = nombre/2
+    except Exception:
+        raise
+    return status
+
 @log_with_args(level=logging.DEBUG)
-async def hello(request):
-    response_obj = {'status': 'pepe'}
+async def success(request):
+    status = calculate(4)
+    response_obj = {'status': int(status)}
+    return web.Response(text=json.dumps(response_obj), status=200, content_type='application/json')
+
+@log_with_args(level=logging.DEBUG)
+async def failure(request):
+    status = calculate('nonumber')
+    response_obj = {'status': int(status)}
     return web.Response(text=json.dumps(response_obj), status=200, content_type='application/json')
 
 
 async def create_api():
     app = web.Application()
     #app.on_startup.append(initialize)
-    app.add_routes([web.get('/', hello)])
+    app.add_routes([web.get('/success', success)])
+    app.add_routes([web.get('/failure', failure)])
 
     runner = web.AppRunner(app)
     await runner.setup()
--- beacon/tests/__main__.py
+++ beacon/tests/__main__.py
@@ -2,7 +2,7 @@ import imp
 from aiohttp.test_utils import TestClient, TestServer, loop_context
 from aiohttp import request
 from aiohttp import web
-from beacon.__main__ import hello
+from beacon.__main__ import success, failure
 from beacon.logs.logs import log_with_args
 import logging
 import json
@@ -11,13 +11,27 @@ import unittest
 def create_app():
     app = web.Application()
     #app.on_startup.append(initialize)
-    app.add_routes([web.get('/', hello)])
+    app.add_routes([web.get('/success', success)])
+    app.add_routes([web.get('/failure', failure)])
     return app
 
 # loop_context is provided as a utility. You can use any
 # asyncio.BaseEventLoop class in its place.
 class TestApp(unittest.TestCase):
-    def test_api(self):
+    def test_api_success(self):
+        with loop_context() as loop:
+            app = create_app()
+            client = TestClient(TestServer(app), loop=loop)
+            loop.run_until_complete(client.start_server())
+            @log_with_args(level=logging.DEBUG)
+            async def test_get_route():
+                resp = await client.get("/success")
+                assert resp.status == 200
+                text = await resp.text()
+                assert json.dumps({'status': 2}) == text
+            loop.run_until_complete(test_get_route())
+            loop.run_until_complete(client.close())
+    def test_api_failure(self):
         with loop_context() as loop:
             app = create_app()
             client = TestClient(TestServer(app), loop=loop)
@@ -25,10 +39,23 @@ class TestApp(unittest.TestCase):
 
             @log_with_args(level=logging.DEBUG)
             async def test_get_route():
-                resp = await client.get("/")
+                resp = await client.get("/failure")
+                assert resp.status == 500
+
+            loop.run_until_complete(test_get_route())
+            loop.run_until_complete(client.close())
+    def test_api_assert_failure(self):
+        with loop_context() as loop:
+            app = create_app()
+            client = TestClient(TestServer(app), loop=loop)
+            loop.run_until_complete(client.start_server())
+
+            @log_with_args(level=logging.DEBUG)
+            async def test_get_route():
+                resp = await client.get("/success")
                 assert resp.status == 200
                 text = await resp.text()
-                assert json.dumps({'status': 'pepe'}) == text
+                assert json.dumps({'status': 3}) == text
 
             loop.run_until_complete(test_get_route())
             loop.run_until_complete(client.close())
