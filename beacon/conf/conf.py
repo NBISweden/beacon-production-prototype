d712113331a514f3845f0448452d194b9c87e060
Joakim Bygdell <joakim.bygdell@nbis.se>
2024-10-17 08:49:42 +0200

    Add option to disable file bases logging
    This is needed when running as unprivileged user or with a read only file system

--- beacon/conf/conf.py
+++ beacon/conf/conf.py
@@ -11,6 +11,7 @@ except Exception as e:# pragma: no cover
     raise_exception(err, errcode)
 
 level=logging.NOTSET
+log_file='beacon/logs/logs.log'
 api_version='2.0.0'
 default_beacon_granularity='record'
 beacon_id = 'org.ega-archive.beacon-ri-demo'  # ID of the Beacon
