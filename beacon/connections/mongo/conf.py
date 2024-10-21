da2a82978a14613f679df32bfcb443cd60d25d13
Hans-Chrstian <hanschristian.vanderwerf@health-ri.nl>
2024-10-21 16:06:35 +0200

    revert conf

--- beacon/connections/mongo/conf.py
+++ beacon/connections/mongo/conf.py
@@ -1,9 +1,8 @@
-database_host = 'healthri-beacon.global.mongocluster.cosmos.azure.com' #'host.docker.internal'
+database_host = 'mongo' #'host.docker.internal'
 database_port = 27017
-database_user = 'healthri'
-database_password = 'bikfab-koxviz-wamDi2'
+database_user = 'root'
+database_password = 'example'
 database_name = 'beacon'
 database_auth_source = 'admin'
 database_certificate = ''
 database_cafile = ''
-database_cluster = True
