d8d5aba850b20c2cf26969b1aa043ea48b12a326
costero-e <orildsaga@gmail.com>
2024-10-22 11:07:00 +0200

    adding github action for updating api version

--- beacon/conf/conf.py
+++ beacon/conf/conf.py
@@ -12,7 +12,6 @@ except Exception as e:# pragma: no cover
 
 level=logging.NOTSET
 log_file='beacon/logs/logs.log'
-api_version='2.0.0'
 beacon_id = 'org.ega-archive.beacon-ri-demo'  # ID of the Beacon
 beacon_name = 'Beacon Reference Implementation demo'  # Name of the Beacon service
 api_version = 'v2.0.0' # Version of the Beacon implementation
